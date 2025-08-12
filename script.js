237ea5c17d51b5d05b3060f6f80bd5070fc362f4
pat <patrickfavret1@gmail.com>
2025-08-12 15:44:22 -0400

    Basic styling and html setup

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("heyo");
+
+const form = document.querySelector("#form");
+
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const postal = document.querySelector("#postal");
+const password = document.querySelector("#password");
+const confirmPWD = document.querySelector("#confirmPWD");
