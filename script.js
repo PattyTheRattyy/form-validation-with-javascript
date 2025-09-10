89be167e717f35bd3c2a89bb1d9bed36aa569dfa
pat <patrickfavret1@gmail.com>
2025-09-10 11:05:56 -0400

    making progress on validation

--- script.js
+++ script.js
@@ -7,3 +7,99 @@ const country = document.querySelector("#country");
 const postal = document.querySelector("#postal");
 const password = document.querySelector("#password");
 const confirmPWD = document.querySelector("#confirmPWD");
+
+email.addEventListener("input", (e) => {
+  checkEmail();
+});
+
+function checkEmail() {
+  if (email.checkValidity()) {
+    email.setCustomValidity("");
+  } else if (email.validity.typeMismatch) {
+    email.setCustomValidity("This is not an email. (example@domain.com)");
+  } else {
+    email.setCustomValidity("");
+  }
+  email.reportValidity();
+}
+
+country.addEventListener("input", (e) => {
+  checkCountry();
+});
+
+function checkCountry() {
+  if (country.checkValidity()) {
+    country.setCustomValidity("");
+  } else if (country.validity.typeMismatch) {
+    country.setCustomValidity("This is not an country.");
+  } else {
+    country.setCustomValidity("");
+  }
+  country.reportValidity();
+}
+
+postal.addEventListener("input", (e) => {
+  checkPostal();
+});
+
+function checkPostal() {
+  if (country.value == "USA") {
+    console.log("USA!!");
+    postal.type = "number";
+    if (postal.value.length != 5) {
+      postal.setCustomValidity(
+        "postal codes in the US should have a length of 5"
+      );
+    } else if (postal.validity.typeMismatch) {
+      postal.setCustomValidity("Numbers only.");
+    }
+  } else if (country.value == "CAN") {
+    console.log("CANADA!!");
+    if (postal.value.length != 6) {
+      postal.setCustomValidity(
+        "postal codes in Canada should have a length of 6"
+      );
+    }
+  }
+
+  if (postal.checkValidity()) {
+    postal.setCustomValidity("");
+  } else if (postal.validity.typeMismatch) {
+    postal.setCustomValidity("This is not a postal code.");
+  } else {
+    postal.setCustomValidity("");
+  }
+  postal.reportValidity();
+}
+
+password.addEventListener("input", (e) => {
+  checkPassword();
+});
+
+function checkPassword() {
+  if (password.checkValidity()) {
+    password.setCustomValidity("");
+  } else if (password.validity.typeMismatch) {
+    password.setCustomValidity("This is not a password.");
+  } else {
+    password.setCustomValidity("");
+  }
+  password.reportValidity();
+}
+
+confirmPWD.addEventListener("input", (e) => {
+  confirmPassword();
+});
+
+function confirmPassword() {
+  if (confirmPWD.value != password.value) {
+    confirmPWD.setCustomValidity("Passwords do not match!");
+  } else if (confirmPWD.checkValidity()) {
+    confirmPWD.setCustomValidity("");
+  } else if (confirmPWD.validity.typeMismatch) {
+    confirmPWD.setCustomValidity("This is not a password.");
+  } else {
+    confirmPWD.setCustomValidity("");
+  }
+  confirmPWD.reportValidity();
+}
